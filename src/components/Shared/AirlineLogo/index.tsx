d792cb006e9702f42e62a5ed946d8e2328a64341
Владислав В. Деханов <vladdex@mail.ru>
2019-07-08 01:42:03 +0300

    Using secure connection on load of airlines logo

--- src/components/Shared/AirlineLogo/index.tsx
+++ src/components/Shared/AirlineLogo/index.tsx
@@ -6,5 +6,5 @@ import {AirlineLogoProps} from './types';
  * Лого авиакомпании ;-)
  */
 export const AirlineLogo: React.FC<AirlineLogoProps> = ({iata, height = 50, width = 150, className}) => (
-	<img src={`http://pics.avs.io/${width}/${height}/${iata}.png`} alt={iata} className={className}/>
+	<img src={`https://pics.avs.io/${width}/${height}/${iata}.png`} alt={iata} className={className}/>
 );
