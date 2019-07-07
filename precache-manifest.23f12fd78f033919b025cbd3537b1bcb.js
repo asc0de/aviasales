df07636e5b372b5ec9f31137eea9a0af836ea664
Владислав В. Деханов <vladdex@mail.ru>
2019-07-07 19:10:00 +0300

    Updates

---
+++ precache-manifest.23f12fd78f033919b025cbd3537b1bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8095ada444a96c6b6b57e7bdd4d40b",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "5acd49c413f7862dc3ed",
+    "url": "/aviasales/static/css/main.8f93554d.chunk.css"
+  },
+  {
+    "revision": "ecd387e424bbeee6e5d6",
+    "url": "/aviasales/static/js/2.03cbf9a0.chunk.js"
+  },
+  {
+    "revision": "5acd49c413f7862dc3ed",
+    "url": "/aviasales/static/js/main.f5f72c22.chunk.js"
+  },
+  {
+    "revision": "af052bb48cec26b9b243",
+    "url": "/aviasales/static/js/runtime~main.f8f776b2.js"
+  },
+  {
+    "revision": "7b1c42c78671de3d9a179ef26220f6ed",
+    "url": "/aviasales/static/media/Logo.7b1c42c7.svg"
+  }
+]);
