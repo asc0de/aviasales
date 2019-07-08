e96e71313e27cfc09ae02fe3371391f1227e020f
Владислав В. Деханов <vladdex@mail.ru>
2019-07-08 09:41:07 +0300

    fix bug with safari

--- src/mappers/ticket.ts
+++ src/mappers/ticket.ts
@@ -2,6 +2,15 @@ import {TicketDto} from '../types/dto';
 import {Ticket} from '../types/models';
 
 /**
+ * Собрать дату в стандартный формат всех браузеров
+ */
+const prepareDateFormat = (date: string): string => {
+	const chunks = date.split('.');
+
+	return `20${chunks[2]}-${chunks[1]}-${chunks[0]}`;
+};
+
+/**
  * Мапит транспортную сущность билета на клиентскую
  */
 export const mapTicket = (ticketDto: TicketDto): Ticket => {
@@ -10,9 +19,9 @@ export const mapTicket = (ticketDto: TicketDto): Ticket => {
 		originName: ticketDto.origin_name,
 		destination: ticketDto.destination,
 		destinationName: ticketDto.destination_name,
-		departureDate: new Date(ticketDto.departure_date),
+		departureDate: new Date(prepareDateFormat(ticketDto.departure_date)),
 		departureTime: ticketDto.departure_time,
-		arrivalDate: new Date(ticketDto.arrival_date),
+		arrivalDate: new Date(prepareDateFormat(ticketDto.arrival_date)),
 		arrivalTime: ticketDto.arrival_time,
 		carrier: ticketDto.carrier,
 		stops: ticketDto.stops,
